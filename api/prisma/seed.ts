d69a526c51e10ad2b88cb117dd0f78160cebd112
Szymon Piątek <szymon.piatek125@gmail.com>
2024-11-17 21:13:51 +0100

    Create Admin User

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -2,10 +2,16 @@ import prisma from '../src/prismaClient';
 import { createRoles } from '../src/fixtures/roles/createRoles';
 import { createPermissions } from '../src/fixtures/permissions/createPermissions';
 import { createPermissionRoles } from '../src/fixtures/permissionRoles/createPermissionRoles';
+import { createAdminUserData } from '../src/fixtures/users/createAdminUserData';
 
 async function main() {
   await Promise.all([...createRoles, ...createPermissions]);
   await createPermissionRoles();
+
+  const adminUserData = await createAdminUserData();
+  await prisma.user.create({
+    data: adminUserData,
+  });
 }
 
 main()
