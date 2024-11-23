ec8d1be898af6c2f4938efad5ea7829025e5e35b
Maciej Slawek <maciejsdev@gmail.com>
2024-11-23 12:40:19 +0100

    Logo, Layout, Links added

---
+++ frontend/src/components/TopBarLayout.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react';
+import { FC } from 'react';
+
+type TopBarProps = {
+    
+  };
+
+export const TopBarLayout: FC<TopBarProps> = (props) => {
+return(
+    <Box></Box>
+)
+
+}
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import { Box } from '@chakra-ui/react';
 import { FC } from 'react';
+import { NavLayout } from '../components/NavLayout';
+import { Outlet } from 'react-router-dom';
 
 export const HomePage: FC<{}> = () => {
-  return <Box>HomePage</Box>;
+  return <NavLayout><Outlet/></NavLayout>;
 };
