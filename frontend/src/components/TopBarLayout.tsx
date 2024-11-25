d4ad29b139459dc0bef62eb63d323bba27455938
Maciek <maciejsdev@gmail.com>
2024-11-25 18:46:15 +0100

    topbar added

--- frontend/src/components/TopBarLayout.tsx
+++ frontend/src/components/TopBarLayout.tsx
@@ -1,5 +1,10 @@
-import { Box } from '@chakra-ui/react';
+import {  Box, Flex, IconButton, Text } from '@chakra-ui/react';
 import { FC } from 'react';
+import { Avatar } from "../components/ui/avatar"
+
+import { MdOutlineAnnouncement,MdSettings, MdPerson  } from "react-icons/md";
+
+
 
 type TopBarProps = {
     
@@ -7,7 +12,35 @@ type TopBarProps = {
 
 export const TopBarLayout: FC<TopBarProps> = (props) => {
 return(
-    <Box>topbar</Box>
-)
-
+    <Flex justify="space-between" p="4" align="center" bg="white" color="black" width="full">
+      {/* NOTES */}
+      <Flex align="center" gap="1">
+        <Box bg="red.600" h="4" w="4" rounded="full"></Box>
+        <Text textStyle="xs">2</Text>
+        <Box bg="yellow.600" h="4" w="4" rounded="full"></Box>
+        <Text textStyle="xs">12</Text>
+        <Box bg="green.600" h="4" w="4" rounded="full"></Box>
+        <Text textStyle="xs">5</Text>
+      </Flex>
+      {/* ICONS AND USER */}
+      <Flex align="center" gap="6">
+        <Box>
+          <IconButton size="xs" >
+            <MdOutlineAnnouncement />
+          </IconButton>
+          <IconButton size="xs" >
+            <MdSettings />
+          </IconButton>
+          <IconButton size="xs" >
+            <MdPerson />
+          </IconButton>
+        </Box>
+        <Flex direction="column">
+         <Text textStyle="sm">John Doe</Text>
+         <Text textStyle="xs" color="gray.500">Admin</Text>
+        </Flex>
+        <Avatar size="xs" name="John Doe" src="https://bit.ly/sage-adebayo" cursor="pointer"/>
+      </Flex>
+    </Flex>
+  )
 }
