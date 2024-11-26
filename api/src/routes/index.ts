0d569f20933e62639388d6ee8c543fa983ce6cf9
Tomasz Kowalczyk <kowalczykt69@gmail.com>
2024-11-26 19:16:33 +0100

    fixed typo

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import userRoutes from '../models/user/routers/userRoutes';
 import authRoutes from '../models/auth/routers/authRoutes';
 import companyRoutes from '../models/company/routers/companyRoutes';
-import eventRoutes from '../models/Event/routers/eventRoutes';
+import eventRoutes from '../models/event/routers/eventRoutes';
 
 const apiRouter = Router();
 
