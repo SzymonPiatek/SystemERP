38a321163703f16774e5f55f99fb7db8441b7a0b
Szymon Piątek <szymon.piatek125@gmail.com>
2024-11-29 18:23:18 +0100

    EnvironmentList

---
+++ frontend/src/components/postman/PostmanSidebar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import CustomButton from '../button/CustomButton.tsx';
+import { Box } from '@chakra-ui/react';
+
+type PostmanSidebarProps = {
+  setActiveSection: (section: string) => void;
+  activeSection: string;
+};
+
+const PostmanSidebar: React.FC<PostmanSidebarProps> = ({ setActiveSection, activeSection }) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap="1rem"
+      height="100dvh"
+      padding="1rem"
+      bg="#111111"
+    >
+      <CustomButton
+        onClick={() => setActiveSection('collections')}
+        variant={activeSection === 'collections' ? 'primary' : 'outline'}
+      >
+        Collections
+      </CustomButton>
+      <CustomButton
+        onClick={() => setActiveSection('environments')}
+        variant={activeSection === 'environments' ? 'primary' : 'outline'}
+      >
+        Environments
+      </CustomButton>
+    </Box>
+  );
+};
+
+export default PostmanSidebar;
--- frontend/src/pages/postman/PostmanPage.tsx
+++ frontend/src/pages/postman/PostmanPage.tsx
@@ -6,11 +6,15 @@ import CustomButton from '../../components/button/CustomButton.tsx';
 import CustomInput from '../../components/input/CustomInput.tsx';
 import { methodCollection } from '../../lib/postmanData.ts';
 import ResponseList from '../../components/postman/ResponseList.tsx';
+import PostmanSidebar from '../../components/postman/PostmanSidebar.tsx';
+import EnvironmentList from '../../components/postman/EnvironmentList.tsx';
 
 const PostmanPage: FC<{}> = () => {
-  const [selectedMethod, setSelectedMethod] = useState<string>('GET');
-  const [selectedUrl, setSelectedUrl] = useState<string>('http://localhost/api/v1');
+  const [selectedMethod, setSelectedMethod] = useState<string>('');
+  const [selectedUrl, setSelectedUrl] = useState<string>('');
   const [activeSection, setActiveSection] = useState<string>('collections');
+  const [activeResponseSection, setActiveResponseSection] = useState<string>('queryParams');
+  const [environmentData, setEnvironmentData] = useState<Record<string, string>>({});
 
   const handleOnSubmit = async () => {
     console.log(selectedMethod);
@@ -19,23 +23,20 @@ const PostmanPage: FC<{}> = () => {
 
   return (
     <Box bg="#222222" minH="100vh" display="flex" gap="1rem">
-      <Box
-        display="flex"
-        flexDirection="column"
-        gap="1rem"
-        height="100dvh"
-        padding="1rem"
-        bg="#111111"
-      >
-        <CustomButton onClick={() => setActiveSection('collections')}>Collections</CustomButton>
-        <CustomButton onClick={() => setActiveSection('environments')}>Environments</CustomButton>
-      </Box>
+      <PostmanSidebar setActiveSection={setActiveSection} activeSection={activeSection} />
 
       {activeSection === 'collections' && (
         <ResponseList setSelectedMethod={setSelectedMethod} setSelectedUrl={setSelectedUrl} />
       )}
 
-      <Box flex="1" padding="1rem" bg="#333333">
+      {activeSection === 'environments' && (
+        <EnvironmentList
+          setEnvironmentData={setEnvironmentData}
+          environmentData={environmentData}
+        />
+      )}
+
+      <Box display="flex" flexDirection="column" flex="1" padding="1rem" bg="#333333" gap="1rem">
         <ClientFormWrapper onSubmit={handleOnSubmit}>
           <Box display="flex" gap="1rem">
             <CustomSelect
@@ -48,10 +49,33 @@ const PostmanPage: FC<{}> = () => {
               value={selectedUrl}
               onChange={(newValue) => setSelectedUrl(newValue as string)}
               style={{ borderColor: 'black', background: 'white' }}
+              placeholder="http://localhost/api/v1"
             />
             <CustomButton type="submit">Send</CustomButton>
           </Box>
         </ClientFormWrapper>
+        <Box display="flex" flexDirection="column" gap="1rem">
+          <Box display="flex" justifyContent="start" gap="1rem">
+            <CustomButton
+              variant={activeResponseSection === 'queryParams' ? 'primary' : 'outline'}
+              onClick={() => setActiveResponseSection('queryParams')}
+            >
+              Query Params
+            </CustomButton>
+            <CustomButton
+              variant={activeResponseSection === 'headers' ? 'primary' : 'outline'}
+              onClick={() => setActiveResponseSection('headers')}
+            >
+              Headers
+            </CustomButton>
+            <CustomButton
+              variant={activeResponseSection === 'json' ? 'primary' : 'outline'}
+              onClick={() => setActiveResponseSection('json')}
+            >
+              JSON
+            </CustomButton>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
