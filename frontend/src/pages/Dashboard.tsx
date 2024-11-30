9ee142587f5cb999e8c88a38964309e072484f82
Polo921 <mpolowczyk@gmail.com>
2024-11-30 12:57:06 +0100

    CreatedListItem

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,14 @@
 import { Box } from '@chakra-ui/react';
 import { FC } from 'react';
+import ItemList from '../components/list/ItemList';
 
 export const Dashboard: FC<{}> = () => {
-  return <Box>Dashboard</Box>;
+  return (
+    <Box>
+      Dashboard
+      <Box>
+        <ItemList />
+      </Box>
+    </Box>
+  );
 };
