87a8ed26a8bcda91f0385f897a7a1c269243cbac
Szymon Piątek <szymon.piatek125@gmail.com>
2024-12-01 19:32:04 +0100

    EnvironmentSection

--- frontend/src/pages/postman/PostmanPage.tsx
+++ frontend/src/pages/postman/PostmanPage.tsx
@@ -4,21 +4,28 @@ import ResponseList from '../../components/postman/list/ResponseList.tsx';
 import PostmanSidebar from '../../components/postman/sidebar/PostmanSidebar.tsx';
 import EnvironmentList from '../../components/postman/list/EnvironmentList.tsx';
 import PostmanMainSection from '../../components/postman/section/PostmanMainSection.tsx';
+import EnvironmentSection from '../../components/postman/section/EnvironmentSection.tsx';
 
 const PostmanPage: FC<{}> = () => {
   // Layout
   const [activeSection, setActiveSection] = useState<string>('collections');
   const [activeResponseSection, setActiveResponseSection] = useState<string>('params');
+  const [activeMainSection, setActiveMainSection] = useState<string>('response');
 
   // Environment
   const [activeEnvironment, setActiveEnvironment] = useState<string>('admin');
+  const [environmentData, setEnvironmentData] = useState<Record<string, string>>({});
 
   // Collection
   const [activeResponse, setActiveResponse] = useState<string>('Login');
 
   return (
     <Box bg="#222222" minH="100vh" display="flex" gap="1rem">
-      <PostmanSidebar setActiveSection={setActiveSection} activeSection={activeSection} />
+      <PostmanSidebar
+        setActiveSection={setActiveSection}
+        activeSection={activeSection}
+        setActiveMainSection={setActiveMainSection}
+      />
 
       {activeSection === 'collections' && (
         <ResponseList setActiveResponse={setActiveResponse} activeResponse={activeResponse} />
@@ -28,15 +35,25 @@ const PostmanPage: FC<{}> = () => {
         <EnvironmentList
           setActiveEnvironment={setActiveEnvironment}
           activeEnvironment={activeEnvironment}
+          setActiveMainSection={setActiveMainSection}
         />
       )}
 
-      <PostmanMainSection
-        activeResponseSection={activeResponseSection}
-        setActiveResponseSection={setActiveResponseSection}
-        activeResponse={activeResponse}
-        activeEnvironment={activeEnvironment}
-      />
+      {activeMainSection === 'response' && (
+        <PostmanMainSection
+          activeResponseSection={activeResponseSection}
+          setActiveResponseSection={setActiveResponseSection}
+          activeResponse={activeResponse}
+          activeEnvironment={activeEnvironment}
+        />
+      )}
+
+      {activeMainSection === 'environment' && (
+        <EnvironmentSection
+          setEnvironmentData={setEnvironmentData}
+          environmentData={environmentData}
+        />
+      )}
     </Box>
   );
 };
