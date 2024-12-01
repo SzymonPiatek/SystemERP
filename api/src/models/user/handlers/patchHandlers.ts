a968ffee057f81683dee5fecbae9f0f9a60cca07
Szymon Piątek <szymon.piatek125@gmail.com>
2024-12-01 10:08:32 +0100

    User - edit user data

--- api/src/models/user/handlers/patchHandlers.ts
+++ api/src/models/user/handlers/patchHandlers.ts
@@ -3,7 +3,7 @@ import prisma from '../../../prismaClient';
 import { comparePassword, hashPassword } from '../../../modules/authModule';
 import { returnError } from '../../../utils/error';
 
-export const changeUserPassword: RequestHandler = async (req, res): Promise<void> => {
+export const changeUserPasswordHandler: RequestHandler = async (req, res): Promise<void> => {
   const { oldPassword, newPassword } = req.body;
   const { id } = req.params;
 
@@ -34,6 +34,42 @@ export const changeUserPassword: RequestHandler = async (req, res): Promise<void
     });
 
     res.status(200).json({ success: true, message: 'Password updated successfully' });
+  } catch (error) {
+    returnError(res, error);
+  }
+};
+
+export const editUserDataHandler: RequestHandler = async (req, res): Promise<void> => {
+  const { id } = req.params;
+  const { firstName, lastName, email } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+    if (!user) {
+      res.status(404).json({ success: false, message: 'User not found' });
+      return;
+    }
+
+    if (email) {
+      const existingUserWithEmail = await prisma.user.findUnique({ where: { email } });
+      if (existingUserWithEmail && existingUserWithEmail.id !== user.id) {
+        res.status(400).json({ success: false, message: 'Email must be unique' });
+        return;
+      }
+    }
+
+    const updatedData: { firstName?: string; lastName?: string; email?: string } = {};
+    if (firstName) updatedData.firstName = firstName;
+    if (lastName) updatedData.lastName = lastName;
+    if (email) updatedData.email = email;
+
+    const updatedUser = await prisma.user.update({
+      where: { id: Number(id) },
+      data: updatedData,
+    });
+
+    res.status(200).json({ success: true, message: 'User updated successfully', user: updatedUser });
+    return;
   } catch (error) {
     returnError(res, error);
   }
--- api/src/models/user/routers/userRoutes.ts
+++ api/src/models/user/routers/userRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { getAllUsersHandler, getUserByIdHandler } from '../handlers/getHandlers';
 import { authenticateToken } from '../../../middlewares/authMiddleware';
-import { changeUserPassword } from '../handlers/patchHandlers';
+import { changeUserPasswordHandler, editUserDataHandler } from '../handlers/patchHandlers';
 
 const router = Router();
 
 router.get('/', authenticateToken, getAllUsersHandler);
 router.get('/:id', authenticateToken, getUserByIdHandler);
 
-router.patch('/:id/change_password', authenticateToken, changeUserPassword);
+router.patch('/:id', authenticateToken, editUserDataHandler);
+router.patch('/:id/change_password', authenticateToken, changeUserPasswordHandler);
 
 export default router;
