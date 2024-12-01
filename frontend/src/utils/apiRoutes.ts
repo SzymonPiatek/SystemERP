59bc6ca10ad77a87a0282ca04fdecea5b9405c9c
Szymon Piątek <szymon.piatek125@gmail.com>
2024-12-01 18:35:15 +0100

    update apiRoutes

--- frontend/src/utils/apiRoutes.ts
+++ frontend/src/utils/apiRoutes.ts
@@ -6,6 +6,8 @@ const API = {
   users: {
     all: '/users',
     user: (id: number) => `/users/${id}`,
+    changeActive: (id: number) => `/users/${id}/change_active`,
+    changePassword: (id: number) => `/users/${id}/change_password`,
   },
   companies: {
     all: '/companies',
