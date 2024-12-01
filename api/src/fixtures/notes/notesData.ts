8ab8f7da6159edfcd46b1db3f5833bf213c80502
Szymon Piątek <119783254+SzymonPiatek@users.noreply.github.com>
2024-12-01 16:53:47 +0100

    Note model & prisma seed (#21)
    * Better notes model
    
    * Prisma seed update
    
    * Better prisma seed
    
    * Note isActive=true default

--- api/src/fixtures/notes/notesData.ts
+++ api/src/fixtures/notes/notesData.ts
@@ -1,37 +1,31 @@
-export const notesData = [
-  {
-    id: 1,
-    title: 'Note 1',
-    description: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-    isActive: true,
-    ownerId: 1,
-  },
-  {
-    id: 2,
-    title: 'Note 2',
-    description: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-    isActive: true,
-    ownerId: 1,
-  },
-  {
-    id: 3,
-    title: 'Note 3',
-    description: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-    isActive: true,
-    ownerId: 1,
-  },
-  {
-    id: 4,
-    title: 'Note 4',
-    description: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-    isActive: true,
-    ownerId: 1,
-  },
-  {
-    id: 5,
-    title: 'Note 5',
-    description: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-    isActive: true,
-    ownerId: 1,
-  },
-];
+import { getISODateTimeWithOffset, isWeekday } from '../../utils/date';
+import { getRandomInt } from '../../utils/number';
+
+export const notesData = (() => {
+  const notes = [];
+  let currentDate = new Date();
+  let dayOffset = 0;
+  let noteCount = 0;
+
+  while (noteCount < 50) {
+    if (isWeekday(currentDate)) {
+      const hasDate = getRandomInt(0, 1) === 1;
+
+      notes.push({
+        id: noteCount + 1,
+        title: `Note ${noteCount + 1}`,
+        description: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
+        isActive: true,
+        ownerId: 1,
+        date: hasDate ? getISODateTimeWithOffset(dayOffset, 0, 0) : null,
+      });
+
+      noteCount++;
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1);
+    dayOffset++;
+  }
+
+  return notes;
+})();
