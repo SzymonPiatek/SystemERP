ef4bf18e016c127f574353c8761c8d3c911a5a11
Szymon Piątek <szymon.piatek125@gmail.com>
2024-12-01 09:57:21 +0100

    User - change password

--- api/src/models/user/routers/userRoutes.ts
+++ api/src/models/user/routers/userRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { getAllUsersHandler, getUserByIdHandler } from '../handlers/getHandlers';
 import { authenticateToken } from '../../../middlewares/authMiddleware';
+import { changeUserPassword } from '../handlers/patchHandlers';
 
 const router = Router();
 
 router.get('/', authenticateToken, getAllUsersHandler);
 router.get('/:id', authenticateToken, getUserByIdHandler);
+
+router.patch('/:id/change_password', authenticateToken, changeUserPassword);
 
 export default router;
