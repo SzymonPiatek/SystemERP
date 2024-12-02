62ff26f9719c7393c3000373638ee091bfa8d7a0
Szymon Piątek <119783254+SzymonPiatek@users.noreply.github.com>
2024-12-02 08:24:33 +0100

    fix getEntity response and update apiRoutes (#25)
    * Fix getEvents and delete console.logs
    
    * update apiRoutes

--- frontend/src/actions/eventActions.ts
+++ frontend/src/actions/eventActions.ts
@@ -2,10 +2,4 @@ import axiosFetch from '../utils/axiosFetch';
 import API from '../utils/apiRoutes';
 import { Event } from '../utils/types';
 
-type EventResponse = {
-  success: boolean;
-  message: string;
-  events: Event[];
-};
-
-export const getEvents = async () => axiosFetch<{ data: EventResponse }>({ url: API.events.all });
+export const getEvents = async () => axiosFetch<{ event: Event[] }>({ url: API.events.all });
