d2dc1c32134a44774c3ae2b0010ff406e299fc34
Szymon Piątek <119783254+SzymonPiatek@users.noreply.github.com>
2024-12-02 08:24:20 +0100

    User active handler (#23)
    * User - change active
    
    * Swagger

--- api/src/models/user/handlers/patchHandlers.ts
+++ api/src/models/user/handlers/patchHandlers.ts
@@ -77,3 +77,31 @@ export const editUserDataHandler: RequestHandler = async (req, res): Promise<voi
     returnError(res, error);
   }
 };
+
+export const changeUserIsActiveHandler: RequestHandler = async (req, res): Promise<void> => {
+  const { id } = req.params;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+    if (!user) {
+      res.status(404).json({ success: false, message: 'User not found' });
+      return;
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id: Number(id) },
+      data: {
+        isActive: !user.isActive,
+      },
+    });
+
+    const safeData = excludePassword(updatedUser);
+
+    const message = safeData.isActive ? 'User activated successfully' : 'User deactivated successfully';
+
+    res.status(200).json({ success: true, message: message, user: safeData });
+    return;
+  } catch (error) {
+    returnError(res, error);
+  }
+};
