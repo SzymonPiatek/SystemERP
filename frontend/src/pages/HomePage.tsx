00223d90e34498c628e97baf55bcd37b86ea7326
Szymon Piątek <szymon.piatek125@gmail.com>
2024-12-14 07:35:54 +0100

    Checkpoint

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom';
 export const HomePage: FC<{}> = () => {
   return (
     <NavLayout>
-      <Box color="black" p="4">
+      <Box color="black" p="4" h="full">
         <Outlet />
       </Box>
     </NavLayout>
