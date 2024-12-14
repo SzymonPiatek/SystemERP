44d1ffa1c7a8965cf5c3b1f779dead66bcd51cb3
Szymon Piątek <szymon.piatek125@gmail.com>
2024-12-14 08:24:51 +0100

    Limiter middleware

--- api/src/models/event/routers/eventRoutes.ts
+++ api/src/models/event/routers/eventRoutes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import { authenticateToken } from '../../../middlewares/authMiddleware';
 import { getAllEventsHandler, getEventByIdHandler } from '../handlers/getHandlers';
 import { postEventHandler } from '../handlers/postHandlers';
+import { apiLimiter, authLimiter } from '../../../middlewares/limiterMiddleware';
 
 const router = Router();
 
-router.get('/', authenticateToken, getAllEventsHandler);
-router.get('/:id', authenticateToken, getEventByIdHandler);
+router.get('/', apiLimiter, authenticateToken, getAllEventsHandler);
+router.get('/:id', apiLimiter, authenticateToken, getEventByIdHandler);
 
-router.post('/', authenticateToken, postEventHandler);
+router.post('/', authLimiter, authenticateToken, postEventHandler);
 
 export default router;
--- api/src/models/user/routers/userRoutes.ts
+++ api/src/models/user/routers/userRoutes.ts
@@ -2,14 +2,15 @@ import { Router } from 'express';
 import { getAllUsersHandler, getUserByIdHandler } from '../handlers/getHandlers';
 import { authenticateToken } from '../../../middlewares/authMiddleware';
 import { changeUserIsActiveHandler, changeUserPasswordHandler, editUserDataHandler } from '../handlers/patchHandlers';
+import { apiLimiter, authLimiter } from '../../../middlewares/limiterMiddleware';
 
 const router = Router();
 
-router.get('/', authenticateToken, getAllUsersHandler);
-router.get('/:id', authenticateToken, getUserByIdHandler);
+router.get('/', apiLimiter, authenticateToken, getAllUsersHandler);
+router.get('/:id', apiLimiter, authenticateToken, getUserByIdHandler);
 
-router.patch('/:id', authenticateToken, editUserDataHandler);
-router.patch('/:id/change_active', authenticateToken, changeUserIsActiveHandler);
-router.patch('/:id/change_password', authenticateToken, changeUserPasswordHandler);
+router.patch('/:id', authLimiter, authenticateToken, editUserDataHandler);
+router.patch('/:id/change_active', authLimiter, authenticateToken, changeUserIsActiveHandler);
+router.patch('/:id/change_password', authLimiter, authenticateToken, changeUserPasswordHandler);
 
 export default router;
