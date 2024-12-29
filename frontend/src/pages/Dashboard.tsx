b4b67e494a9084079ed1ca995a535a68e7b8c89f
Jan <jan69paczuski@gmail.com>
2024-12-29 18:34:45 +0100

    Fix

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,7 +2,6 @@ import { Flex } from '@chakra-ui/react';
 import { FC } from 'react';
 import { EventCalendar } from '../components/calendar/EventCalendar';
 import { DailyCalendar } from '../components/calendar/DailyCalendar';
-import { CardBox } from '../components/Cardbox';
 
 export const Dashboard: FC<{}> = () => {
   return (
