bba572038f7692065394eeb588df5b6eb8cfc21d
Szymon Piątek <szymon.piatek125@gmail.com>
2025-02-09 18:47:20 +0100

    swagger docs

---
+++ api/src/models/user/swagger/sendResetPasswordHandler.swagger.ts
@@ -0,0 +1,73 @@
+/**
+ * @swagger
+ * /users/forgot_password:
+ *   post:
+ *     summary: Send password reset email
+ *     tags: [Users]
+ *     description: Allows a user to request a password reset link via email.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: "user@example.com"
+ *                 description: The email address associated with the user's account.
+ *     responses:
+ *       '200':
+ *         description: Password reset email successfully sent.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: "Password reset email sent"
+ *       '400':
+ *         description: Bad request due to invalid email or failure in sending email.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Error while sending email"
+ *       '404':
+ *         description: User not found for the given email.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "User not found"
+ *       '500':
+ *         description: Internal server error while processing the request.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Internal Server Error"
+ */
