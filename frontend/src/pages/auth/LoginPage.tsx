a554d7ebebbd9dba52d9e103a819b5bf9df1f199
Maciek <maciejsdev@gmail.com>
2025-02-10 08:48:41 +0100

    Login section

--- frontend/src/pages/auth/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -1,9 +1,10 @@
 import { FC, useState } from 'react';
 import ClientFormWrapper from '../../components/form/formWrapper/ClientFormWrapper.tsx';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Input, Grid, GridItem, Field } from '@chakra-ui/react';
 import { useLoginUser } from '../../hooks/useLoginUser.tsx';
-import CustomInput from '../../components/input/CustomInput.tsx';
 import CustomButton from '../../components/button/CustomButton.tsx';
+import { Logo } from '../../components/logo/Logo.tsx';
+import { PasswordInput } from '../../components/ui/password-input.tsx';
 
 export const LoginPage: FC = () => {
   const [email, setEmail] = useState<string>('');
@@ -16,46 +17,63 @@ export const LoginPage: FC = () => {
   };
 
   return (
-    <Box
+    <Grid
+      templateColumns={{ base: '1fr', lg: '1fr 1fr' }}
       width="100dvw"
       height="100dvh"
-      display="flex"
-      justifyContent="center"
+      gap="0"
       alignItems="center"
-      background="gray.100"
+      justifyContent="center"
     >
-      <Box width="100%" maxWidth="400px" display="flex" flexDirection="column" gap="2rem">
-        <Box width="100%" display="flex" justifyContent="center">
-          <Heading size="3xl" color="black">
-            Login
-          </Heading>
-        </Box>
-        <ClientFormWrapper onSubmit={handleLogin}>
-          <Box display="flex" flexDirection="column" gap="1rem">
-            <CustomInput
-              type="email"
-              name="email"
-              required
-              value={email}
-              onChange={(value) => setEmail(value as string)}
-              placeholder="Email"
-            />
-            <CustomInput
-              type="password"
-              name="password"
-              required
-              value={password}
-              onChange={(value) => setPassword(value as string)}
-              placeholder="Password"
-            />
-            <Box width="100%" display="flex" justifyContent="center" marginTop="1rem">
-              <CustomButton type="submit" variant="primary">
-                Sign in
-              </CustomButton>
-            </Box>
+      {/* Logo */}
+      <GridItem
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        mx={{ base: 20, lg: 'none' }}
+      >
+        <Logo />
+      </GridItem>
+
+      {/* Login form */}
+      <GridItem display="flex" justifyContent="center">
+        <Box width="100%" maxWidth="400px" display="flex" flexDirection="column" gap="2rem">
+          <Box width="100%" display="flex" justifyContent="center">
+            <Heading size="3xl">Login</Heading>
           </Box>
-        </ClientFormWrapper>
-      </Box>
-    </Box>
+          <ClientFormWrapper onSubmit={handleLogin}>
+            <Box display="flex" flexDirection="column" gap="1rem">
+              <Field.Root>
+                <Field.Label>Email</Field.Label>
+                <Input
+                  type="email"
+                  name="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="me@example.com"
+                />
+              </Field.Root>
+              <Field.Root>
+                <Field.Label>Password</Field.Label>
+                <PasswordInput
+                  type="password"
+                  name="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                />
+              </Field.Root>
+              <Box width="100%" display="flex" justifyContent="center" marginTop="1rem">
+                <CustomButton type="submit" variant="primary">
+                  Sign in
+                </CustomButton>
+              </Box>
+            </Box>
+          </ClientFormWrapper>
+        </Box>
+      </GridItem>
+    </Grid>
   );
 };
