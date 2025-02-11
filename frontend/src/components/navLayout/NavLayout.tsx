d17d44354c7bfc85b7720ddf2017dcc261a671f1
Maciej Sławek <75547254+Maciejsdev@users.noreply.github.com>
2025-02-11 17:56:03 +0100

    NE-24 layout (#50)
    * Layout refactored
    
    * Login section
    
    * small fixes

--- frontend/src/components/navLayout/NavLayout.tsx
+++ frontend/src/components/navLayout/NavLayout.tsx
@@ -1,65 +1,35 @@
-import { Box, Stack, Flex, VStack } from '@chakra-ui/react';
-import { FC, PropsWithChildren, useContext } from 'react';
-import { Logo } from '../logo/Logo.tsx';
-import { TopBarLayout } from '../topBarLayout/TopBarLayout.tsx';
-import { NavLink } from './NavLink.tsx';
+import { Box, Stack, Flex } from '@chakra-ui/react';
+import { FC, PropsWithChildren } from 'react';
 import { MdDashboard, MdCalendarMonth, MdPeopleAlt, MdNotes, MdChecklist } from 'react-icons/md';
+import { NavLink } from './NavLink.tsx';
 import { TextWithIcon } from './TextWithIcon';
 import NavFooter from './NavFooter.tsx';
-import { AuthContext } from '../../contexts/AuthContext.tsx';
 
 export type NavLayoutProps = PropsWithChildren<{}>;
 
-export const NavLayout: FC<NavLayoutProps> = (props) => {
-  const { children } = props;
-  const { user } = useContext(AuthContext);
-
+export const NavLayout: FC<NavLayoutProps> = () => {
   return (
-    <Flex flex="1">
-      <Stack
-        as="aside"
-        bg={{ base: 'white', _dark: 'black' }}
-        width={['15%', '13%']}
-        minH="100vh"
-        position="fixed"
-        left="0"
-        top="0"
-        justifyContent="space-between"
-      >
-        <Box>
-          <Box h="4rem" display="flex" placeItems="center">
-            <Logo />
-          </Box>
-          <Stack as="nav" px={4} pb={8}>
-            <NavLink to="/">
-              <TextWithIcon title="Dashboard" Icon={MdDashboard}></TextWithIcon>
-            </NavLink>
-            <NavLink to="/schedule">
-              <TextWithIcon title="Schedule" Icon={MdCalendarMonth}></TextWithIcon>
-            </NavLink>
-            <NavLink to="/users">
-              <TextWithIcon title="Users" Icon={MdPeopleAlt}></TextWithIcon>
-            </NavLink>
-            <NavLink to="/notes">
-              <TextWithIcon title="Notes" Icon={MdNotes}></TextWithIcon>
-            </NavLink>
-            <NavLink to="/orders">
-              <TextWithIcon title="Orders" Icon={MdChecklist}></TextWithIcon>
-            </NavLink>
-          </Stack>
-        </Box>
-        <Box p={4}>
-          <NavFooter />
-        </Box>
+    <Flex direction="column" height="100%">
+      <Stack mx={4} flex="1">
+        <NavLink to="/">
+          <TextWithIcon title="Dashboard" Icon={MdDashboard} />
+        </NavLink>
+        <NavLink to="/schedule">
+          <TextWithIcon title="Schedule" Icon={MdCalendarMonth} />
+        </NavLink>
+        <NavLink to="/users">
+          <TextWithIcon title="Users" Icon={MdPeopleAlt} />
+        </NavLink>
+        <NavLink to="/notes">
+          <TextWithIcon title="Notes" Icon={MdNotes} />
+        </NavLink>
+        <NavLink to="/orders">
+          <TextWithIcon title="Orders" Icon={MdChecklist} />
+        </NavLink>
       </Stack>
-
-      <VStack ml={['15%', '13%']} width="full" height="100vh" overflow="hidden">
-        {user && <TopBarLayout user={user} />}
-
-        <Box flex="1" width="100%" overflowY="auto">
-          {children}
-        </Box>
-      </VStack>
+      <Box mt="auto">
+        <NavFooter />
+      </Box>
     </Flex>
   );
 };
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,27 @@
-import { Box } from '@chakra-ui/react';
+import { GridItem, Grid } from '@chakra-ui/react';
 import { FC } from 'react';
 import { NavLayout } from '../components/navLayout/NavLayout.tsx';
 import { Outlet } from 'react-router-dom';
+import { TopBarLayout } from '../components/topBarLayout/TopBarLayout.tsx';
 
 export const HomePage: FC<{}> = () => {
   return (
-    <Box w="100vw" h="100vh" overflow="hidden">
-      <NavLayout>
-        <Box flex="1" color="black" p="4" bg={{ base: 'gray.50', _dark: 'gray.800' }} minH="full">
-          <Outlet />
-        </Box>
-      </NavLayout>
-    </Box>
+    <Grid
+      templateRows="auto 1fr"
+      templateColumns={{ base: '80px 1fr', lg: '200px 1fr' }}
+      height="100vh"
+    >
+      <GridItem as="header" colSpan={2}>
+        <TopBarLayout />
+      </GridItem>
+
+      <GridItem as="nav">
+        <NavLayout />
+      </GridItem>
+
+      <GridItem as="main" p="4" overflowY="auto" bg={{ base: 'gray.50', _dark: 'gray.800' }}>
+        <Outlet />
+      </GridItem>
+    </Grid>
   );
 };
